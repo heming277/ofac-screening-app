2c935df704a400f4f7c2c8096eefed9ab332b937
Heming Liu <hemingliu123@gmail.com>
2024-08-02 15:56:18 -0400

    fix rate limiter

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,24 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
-import { RateLimiter } from 'limiter'
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import { RateLimiter } from 'limiter';
+
+// Check if Date.now() is available, provide a fallback if not
+if (typeof Date.now === 'undefined') {
+  Date.now = () => new Date().getTime();
+}
 
 const limiter = new RateLimiter({
   tokensPerInterval: 10,
   interval: 'minute',
   fireImmediately: true,
-})
+});
 
 export async function middleware(request: NextRequest) {
   if (request.nextUrl.pathname === '/api/screen') {
-    const remaining = await limiter.removeTokens(1)
+    const remaining = await limiter.removeTokens(1);
     if (remaining < 0) {
-      return new NextResponse('Too Many Requests', { status: 429 })
+      return new NextResponse('Too Many Requests', { status: 429 });
     }
   }
-  return NextResponse.next()
+  return NextResponse.next();
 }
